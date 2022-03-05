a5ea22c50437b05fb1bf9b8ff2b079cfb06c1f78
Wael Farhat <65680997+wael7721@users.noreply.github.com>
2022-03-05 21:50:54 +0100

    Add files via upload

--- route/admin.js
+++ route/admin.js
@@ -1,27 +1,38 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
 const { json } = require("express");
+const ver = require("../verifictationenum");
+const mongoose = require("mongoose");
 
 const router = express.Router();
 router.use(express.json());
 
-router.delete("/api/user", async (req, res) => {
-  const token = req.headers["x-access-token"];
-  decoded = jwt.verify(token, "secret123");
-  if (!isAdmin) {
-    res.json({ status: "error", error: "unauthorised access" });
+mongoose.connect("mongodb://127.0.0.1:27017/khademnitn", function (err, db) {
+  if (err) {
+    throw err;
   }
+  console.log("db connected from admin");
+});
+
+router.delete("/user", async (req, res) => {
+//sprint2:delete user
   try {
-    await User.deleteOne({ email: req.body.email });
+    const token = req.headers["x-access-token"];
+    decoded = jwt.verify(token, "secret123");
+    const user = await User.find({ email: decoded.email }, { enum: 1 });
+    if (!user) {
+      return res.json({ error: "token invalid" });
+    }
+    if (user[0].enum != "admin") {
+      return res.json((error = "unauthorized access"));
+    }
+    await User.findOneAndDelete({ email: decoded.email });
+    return res.json({result:"user deleted"})
   } catch (err) {
-    res.json({ status: err, error: "couldnt delete" });
+    res.json({ error: err, message: "couldnt delete" });
   }
 });
-isAdmin = () => {
-  return true;
-};
 
 module.exports = router;
